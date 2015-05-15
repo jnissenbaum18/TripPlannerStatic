4bc98de4f8f456a8f55c3cad09e6e79fb904e522
Catherine Lau <clau.catherine@gmail.com>
2015-05-15 15:33:51 -0400

    Finished?

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -5,9 +5,13 @@ var itineraries = [
     {
         hotels: [],
         restaurants: [],
-        thingsToDo: []
+        thingsToDo: [],
+        markers: []
     }
 ];
+var bounds = new google.maps.LatLngBounds();
+var marker
+
 
 function initialize_gmaps() {
     // initialize new google maps LatLng object
@@ -32,58 +36,153 @@ function initialize_gmaps() {
 
 $(document).ready(function() {
     initialize_gmaps();
+
 });
 
 var buttonMaker = function (value, element) {
 
-    var button = $('<button type="button" class="btn btn-default subtract">-</button>');
+    var button = $('<button type="button" value="'+element+'" class="btn btn-default subtract">-</button>');
     var text = $('<div style="margin-top: 10px" class="subtract"> <div class="list-group-item-text itineraryListItem subtract">' + value[0] + '</div>' + '</div>');
- 
-    // var location = new google.maps.LatLng(value[1],value[2]);
-    // var marker = new google.maps.Marker({
-    //     position: location
-    // });
-    // marker.setMap(map);
 
+    
+
+// delete item click event
     $(element).append(text).append(button);
     button.on('click', function () {
-        text.remove()
-        button.remove()
-        marker.setMap(null);
+        var el = this.value.slice(1,2)
+        // var el = element
+        if (el === 'h') {
+            var ele = 'hotels'
+        }
+        else if (el === 'r') {
+            var ele = 'restaurants'
+        }
+        else if (el ==='t') {
+            var ele = 'thingsToDo'
+        }
+
+        var items = itineraries[currentDay-1][ele];
+
+        for (var i = 0; i < items.length; i++) {
+             if (items[i][0] === value[0]) {
+                items.splice(i, 1);
+                text.remove()
+                button.remove()
+                for (var j = 0; j < itineraries[currentDay-1].markers.length; j++) {
+                     if (itineraries[currentDay-1].markers[j] === marker) {
+                            itineraries[currentDay-1].markers[j].setMap(null);
+                            itineraries[currentDay-1].markers.splice(j,1);
+                     }
+                }  
+                
+                for (var j = 0; j < itineraries[currentDay-1].markers.length; j++) {
+                    bounds.extend(itineraries[currentDay-1].markers[j].position);
+                    map.fitBounds(bounds);
+                }
+                return;
+            }
+        }
+
+        
+        // value[0]
+        
+
     })
+}
+
+var markerMaker = function (value, element) {
+    var location = new google.maps.LatLng(value[1],value[2]);
+    if (element === '#rList') {
+        var icon = {
+            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+            scale: 5
+        }
+    }
+
+    else if (element === '#tList') {
+        var icon = {
+            path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW,
+            scale: 5
+        }
+    }
+
+    marker = new google.maps.Marker({
+        position: location,
+        icon: icon
+    });
+
+    marker.setMap(map);
+    console.log("markers array:" + itineraries[currentDay-1].markers);
+    itineraries[currentDay-1].markers.push(marker); // store marker in day
+
+    bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < itineraries[currentDay-1].markers.length; i++) {
+        bounds.extend(itineraries[currentDay-1].markers[i].position);
+        map.fitBounds(bounds);
+    }
+
+    bounds.extend(marker.position);
 }
 
 $('#hotelButton').on('click', function () {
     var hotelInfo = $('#hotelSelect :selected').val().split(',')
-    buttonMaker(hotelInfo, '#hotelList')
+    buttonMaker(hotelInfo, '#hList')
+    markerMaker(hotelInfo, '#hList')
     itineraries[currentDay-1].hotels.push(hotelInfo);
 });
 
 $('#restButton').click(function() {
     var restaurantInfo = $('#restaurantSelect :selected').val().split(',')
-    buttonMaker(restaurantInfo, '#restaurantList')
+    buttonMaker(restaurantInfo, '#rList')
+    markerMaker(restaurantInfo, '#rList')
     itineraries[currentDay-1].restaurants.push(restaurantInfo);
 });
 
 $('#thingButton').click(function() {
     thingsInfo = $('#thingsToDoSelect :selected').val().split(',');
-    buttonMaker(thingsInfo, '#thingsToDoList')
+    buttonMaker(thingsInfo, '#tList')
+    markerMaker(thingsInfo, '#tList')
     itineraries[currentDay-1].thingsToDo.push(thingsInfo);
 });
 
 // create object that stores an iternary for each day
 
 
+$('#firstDay').on('click', function() {
+
+        $('.subtract').remove();
+        for (var i = 0; i < itineraries[currentDay -1].markers.length; i++) {
+            itineraries[currentDay -1].markers[i].setMap(null);
+        }
+        currentDay = this.value
+        $('#day').text("Day " + currentDay);
+
+        itineraries[currentDay - 1].hotels.forEach(function(hotel) {
+            buttonMaker(hotel, '#hList'); 
+        })
+        itineraries[currentDay - 1].restaurants.forEach(function(restaurant) {
+            buttonMaker(restaurant, '#rList')
+        })
+        itineraries[currentDay - 1].thingsToDo.forEach(function(thing) {
+            buttonMaker(thing, '#tList')
+        })
+    })
 
 // give functionality to our + button 
 
 $('#addDay').on('click', function() {
      // create a new day in our object
+    for (var i = 0; i < itineraries[currentDay -1].markers.length; i++) {
+            itineraries[currentDay -1].markers[i].setMap(null);
+    }
      currentDay = itineraries.length + 1;
+     $('#day').text("Day " + currentDay);
+
      var newDay = {
         hotels: [],
         restaurants: [],
-        thingsToDo: []
+        thingsToDo: [],
+        markers: []
      };
     itineraries.push(newDay);
 
@@ -91,21 +190,58 @@ $('#addDay').on('click', function() {
     var newDayButton = $('<button value="' + currentDay + '"type="button" class="btn btn-default">' + currentDay + '</button>');
     // $('#dayButtons').append(newDayButton)
     $(this).before(newDayButton);
-    newDayButton.on('click', function() {
+    newDayButton.on('click', function() {   // SWITCH DAY EVENT
         $('.subtract').remove();
-        currentDay = this.value
+        for (var i = 0; i < itineraries[currentDay -1].markers.length; i++) {
+            itineraries[currentDay -1].markers[i].setMap(null);
+        }
+
+        currentDay = this.value;
+        $('#day').text("Day " + currentDay);
         itineraries[currentDay - 1].hotels.forEach(function(hotel) {
-            buttonMaker(hotel, '#hotelList'); 
+            buttonMaker(hotel, '#hList'); 
+            markerMaker(hotel, '#hList')
         })
         itineraries[currentDay - 1].restaurants.forEach(function(restaurant) {
-            buttonMaker(restaurant, '#restaurantList')
+            buttonMaker(restaurant, '#rList')
+            markerMaker(restaurant, '#rList')
         })
         itineraries[currentDay - 1].thingsToDo.forEach(function(thing) {
-            buttonMaker(thing, '#thingsToDoList')
+            buttonMaker(thing, '#tList')
+            markerMaker(thing, '#tList')
         })
     })
 });
 
+$('#subtractDay').on('click', function() {
+     $('.subtract').remove();
+     // $('#dayButtons').
+     $('button[value=' + currentDay + ']').remove();
+     currentDay -= 1
+     itineraries.splice(currentDay, 1);
+     for (var i = currentDay; i < itineraries.length + 1; i++) {
+        $('button[value=' + Number(i + 1) + ']').text(i).val(i);
+     }
+    // currentDay = this.value;
+    // $('#day').text("Day " + currentDay);
+    itineraries[currentDay].hotels.forEach(function(hotel) {
+        buttonMaker(hotel, '#hList'); 
+        markerMaker(hotel, '#hList')
+    })
+    itineraries[currentDay].restaurants.forEach(function(restaurant) {
+        buttonMaker(restaurant, '#rList')
+        markerMaker(restaurant, '#rList')
+    })
+    itineraries[currentDay].thingsToDo.forEach(function(thing) {
+        buttonMaker(thing, '#tList')
+        markerMaker(thing, '#tList')
+    })
+     // $('#addDay').siblings().remove();
+     // for (i = currentDay+1; i > 0; i--){
+     //    $('#dayButtons').prepend('<button value="' + i + '"type="button" class="btn btn-default">' + i + '</button>');
+     // }
+
+});
    
 
         // each day will have 3 keys that store arrays
